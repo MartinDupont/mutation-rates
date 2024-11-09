Fixed some errors in the distance matrix calculation

process_phylo_tree.py
@@ -7,8 +7,30 @@ from Bio import Phylo
 from Bio.Phylo.TreeConstruction import DistanceMatrix, DistanceTreeConstructor
 from Levenshtein import distance
 
-from common import make_key, time_it_cumulative, TIME_STORE
+from common import make_key, time_it_cumulative, TIME_STORE, time_it
 from common import parse_fasta_file, translate
+
+
+
+class IdStore():
+    def __init__(self):
+        self.next_id = 0
+        self.store = {}
+
+    def __getitem__(self, item):
+        id = self.store.get(item, None)
+        if id is not None:
+            return id
+
+        retval = self.next_id
+        self.store[item] = retval
+        self.next_id = retval + 1
+        return retval
+
+    def to_reverse_map(self):
+        return {v:k for k, v in self.store.items()}
+
+
 
 def extract_lower_triangular(matrix):
     n = matrix.shape[0]
@@ -24,11 +46,17 @@ def extract_lower_triangular(matrix):
 
 
 @time_it_cumulative
-def prepare_distance_data(records, sample_ids, sequence_store, genes_to_ids, current_id):
+def prepare_distance_data(records, sample_ids, sequence_store: IdStore, genes_to_ids):
     for r in records:
         key = make_key(r)
+        sample_id = r['sample']
         sample_sequence = r.get('sequence')
         if not key or not sample_sequence:
+            continue
+
+        if sample_ids[sample_id].get(key, None) is not None:
+            # Some genomes are listed as having multiple copies of the same gene, according to NCIH
+            # We filter out any extra copies.
             continue
 
         try:
@@ -39,22 +67,36 @@ def prepare_distance_data(records, sample_ids, sequence_store, genes_to_ids, cur
         except ValueError as e:
             continue
 
-        id = sequence_store.get(sample_protein)
-        if id is None:
-            sequence_store[sample_protein] = current_id
-            genes_to_ids[key].add(current_id)
-
-            sample_ids[r['sample']].update({key: current_id})
-            current_id += 1
-        else:
-            genes_to_ids[key].add(id)
-            sample_ids[r['sample']].update({key: id})
-
+        id = sequence_store[sample_protein]
+        genes_to_ids[key].add(id)
+        sample_ids[sample_id].update({key: id})
 
 
 
 @time_it_cumulative
-def calculate_distance_matrix(sample_ids, sequence_store, genes_to_ids):
+def make_reduced_distance_matrix(reversed_sequence_store, genes_to_ids):
+
+    distances = {}
+
+    for key, sample_ids_for_key in genes_to_ids.items():
+        for i in sample_ids_for_key:
+            for j in sample_ids_for_key:
+                if j >= i:
+                    continue
+                s_i = reversed_sequence_store[i]
+                s_j = reversed_sequence_store[j]
+
+                difference = distance(s_i, s_j)
+                # This is doubling up! We should bail out if i = j
+                # And put in a 0 for i == j
+                distances[(i, j)] = difference
+                distances[(j, i)] = difference
+
+    return distances
+
+
+@time_it_cumulative
+def calculate_distance_matrix(sample_ids, sequence_store:IdStore, genes_to_ids):
     """
     We try to calculate an NxN distance matrix.
 
@@ -72,36 +114,27 @@ def calculate_distance_matrix(sample_ids, sequence_store, genes_to_ids):
     :return:
     """
 
-    reversed_sequence_store = {v:k for k, v in sequence_store.items()}
+    reversed_sequence_store = sequence_store.to_reverse_map()
 
-    distances = {}
-
-    for key, sample_ids_for_key in genes_to_ids.items():
-        for i in sample_ids_for_key:
-            for j in sample_ids_for_key:
-                s_i = reversed_sequence_store[i]
-                s_j = reversed_sequence_store[j]
-
-                difference = distance(s_i, s_j)
-                distances[(i, j)] = difference
-                distances[(j, i)] = difference
+    distances = make_reduced_distance_matrix(reversed_sequence_store, genes_to_ids)
 
     dist_matrix = np.zeros((len(sample_ids), len(sample_ids)))
     i = 0
     for s1, ids_1 in sample_ids.items():
         j = 0
         for s2, ids_2 in sample_ids.items():
-            differences = []
             for key, seq_id_1 in ids_1.items():
                 seq_id_2 = ids_2.get(key)
                 if seq_id_2 is None:
                     continue
 
-                difference = distances[(seq_id_1, seq_id_2)]
-                differences += [difference]
+                if seq_id_1 == seq_id_2:
+                    # Distance for identical pairs is 0
+                    continue
 
-            total_distance = sum(differences)
-            dist_matrix[i,j] = total_distance
+                difference = distances[(seq_id_1, seq_id_2)]
+                dist_matrix[i,j] += difference
+
             j += 1
         i += 1
 
@@ -122,11 +155,10 @@ def make_phylogenetic_tree(base_directory, limit =100000):
 
     count = 0
     sample_ids = defaultdict(dict)
-    sequence_store = defaultdict(int)
+    sequence_store = IdStore()
     genes_to_ids = defaultdict(set)
-    current_id = 0
     for folder_name in os.listdir(base_directory):
-        if count > limit:
+        if count >= limit:
             break
         folder_path = os.path.join(base_directory, folder_name)
         if os.path.isdir(folder_path):
@@ -134,7 +166,7 @@ def make_phylogenetic_tree(base_directory, limit =100000):
             if os.path.isfile(file_path):
                 with open(file_path, 'r') as file:
                     parsed = parse_fasta_file(file, folder_name, include_sequences=True)
-                    prepare_distance_data(parsed, sample_ids, sequence_store, genes_to_ids, current_id)
+                    prepare_distance_data(parsed, sample_ids, sequence_store, genes_to_ids)
                     count += 1
 
     pickle.dump(sample_ids, open('sample_ids.pkl', 'wb'))
@@ -156,6 +188,6 @@ if __name__ == '__main__':
     base_directory = '/Users/martin/Documents/data/ncbi_new/ncbi_dataset/ncbi_dataset/data'
 
 
-    dm = make_phylogenetic_tree(base_directory)
+    dm = make_phylogenetic_tree(base_directory, limit=400)
     for key, value in TIME_STORE.items():
         print(f"\t Time for {key}: {value}")