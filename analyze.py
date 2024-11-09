bc29928fd5f3ca66c642e0cea124b37a4ed66003
Martin Dupont <m.dupont@live.com.au>
2024-11-09 16:22:35 -0500

    Tidy up output of analyze script

--- analyze.py
+++ analyze.py
@@ -4,22 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def calculate_mutation_rates(df):
-    df = df[df['n_anomalies'] < 1].copy()
-    #df = df[df['major_allele_freq'] > 0.5].copy()
+def calculate_mutation_rates(df, anomaly_threshold=0, h_null_mutation_rate=None):
+    df = df[df['n_anomalies'] <= anomaly_threshold].copy()
 
-    df = df.drop(columns = ['n_anomalies'])
+    df = df.drop(columns=['n_anomalies'])
 
     # In the math explainer we showed that we can just count the total mutations over the total length
     df['total_len'] = df['sample_count'] * df['ref_length']
 
     df['mutation_rate'] = df['n_mutations'] / (df['total_len'] * df['ref_length'])
 
-    total_mutation_rate = df['n_mutations'].sum() / df['total_len'].sum()
+    if h_null_mutation_rate is None:
+        h_null_mutation_rate = df['n_mutations'].sum() / df['total_len'].sum()
 
-    df['expected_mutations'] = df['total_len'] * total_mutation_rate
+    df['expected_mutations'] = df['total_len'] * h_null_mutation_rate
 
-    df['p_value'] = df.apply(lambda x: poisson.cdf(x['n_mutations'], x['total_len'] * total_mutation_rate), axis=1)
+    df['p_value'] = df.apply(lambda x: poisson.cdf(x['n_mutations'], x['total_len'] * h_null_mutation_rate), axis=1)
 
     df['neg_log_p'] = - np.log10(df['p_value'])
 
@@ -29,56 +29,47 @@ def calculate_mutation_rates(df):
 
 
 def make_summary_plots(df):
-    y = df['neg_log_p'].sort_values()
-
-    x = np.log10(np.arange(len(y)) + 1)
-
-    plt.scatter(x, y)
-    plt.xlabel('-log ( expected pvals )')
-    plt.ylabel('-log ( actual pvals )')
-
-    plt.show()
-    plt.savefig('qq_plot')
-    plt.close()
-
-
     data = df['p_value']
-    plt.hist(data, 100, alpha=0.5, color="black")
+    plt.hist(data, 100, alpha=0.5, color="b")
     plt.xlabel('p-value')
     plt.ylabel('count')
+    plt.title("p-value distribution across genes")
     plt.show()
     plt.close()
 
-
-    # TODO: What am I even trying to show here?
-    data = df[df['p_value'] < 1e-8]['mutation_rate']
-    plt.hist(data, 100, alpha=0.5, color="black")
+    data = df['mutation_rate']
+    plt.hist(data, 40, alpha=0.5, color="b", log=True)
     plt.xlabel('Mutations per base pair')
+    plt.ylabel('Log count of genes')
+    plt.title('Distribution of mutation frequency across genes')
     plt.show()
     plt.close()
-
-
-
 
 
 if __name__ == '__main__':
-
     import pickle
+
     df = pickle.load(open('df', 'rb'))
 
-    print("--------------------------")
-    print(len(df[df['n_anomalies'] == 0]))
-    print(len(df[df['n_anomalies'] < 10]))
-    print(df.sort_values('n_anomalies'))
+    low_mutation_rate = 0.0001
+    df_low = calculate_mutation_rates(df, h_null_mutation_rate=low_mutation_rate)
+    zeros = df_low['p_value'] < 1e-8
+    df_significantly_low = df_low[zeros]
 
-    df = calculate_mutation_rates(df)
+    print('------------------------------------------')
+    print('Genes with anomalously low mutation rates')
+    print('------------------------------------------')
+    print(df_significantly_low)
 
-    #print("-----------------------------")
-    zeros = df['p_value'] == 0
-    ones = df['p_value'] == 1
-    df_significant = df[zeros | ones]
-    pdb.set_trace()
-    #print(df_significant.sort_values('mutation_rate').iloc[-40:])
+    high_mutation_rate = 0.005
+    df_high = calculate_mutation_rates(df, h_null_mutation_rate=high_mutation_rate)
+    ones = df_high['p_value'] > 1 - 1e-8
+    df_significantly_high = df_high[ones]
 
+    print('------------------------------------------')
+    print('Genes with anomalously high mutation rates')
+    print('------------------------------------------')
+    print(df_significantly_high)
 
-    make_summary_plots(df)
+    df_middle = calculate_mutation_rates(df)
+    make_summary_plots(df_middle)
